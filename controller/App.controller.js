55d282cc319d7f278a49e4b6764a8c2b689bf151
rp-honorato <108833431+rp-honorato@users.noreply.github.com>
2025-01-16 18:23:02 +0000

    First Commit

---
+++ controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("crud.northwind.controller.App", {
+      onInit() {
+      }
+  });
+});
